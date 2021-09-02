65c92952d54c8de5c5bc459ff1b1282eb4dbae89
mateomoragues <moraguesmateo@gmail.com>
2021-09-02 09:49:05 -0300

    estructura inicial

---
+++ proyecto-integrador-3/src/components/Content/Content.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Content extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+
+    render(){
+        return(
+
+            <div>
+                <h1>
+                    Hola mundo
+                </h1>
+            </div>
+
+        )
+    }
+}
+
+export default Content;
